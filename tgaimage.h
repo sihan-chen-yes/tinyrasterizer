84dd044f0b62e2b20c23fae502ea04a6be91ddb7
Sihan Chen <sihanchen2001222@gmail.com>
2025-03-15 23:17:30 +0100

    feat: color operation range check

--- tgaimage.h
+++ tgaimage.h
@@ -7,6 +7,7 @@
 #pragma once
 #include <cstdint>
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include "geometry.h"
 // TGA picture rules, can't pad extra bytes
@@ -31,9 +32,71 @@ struct TGAColor {
     std::uint8_t bgra[4] = {0, 0, 0, 0}; // BGRA order
     std::uint8_t bytespp = 4; // bytes per pixel
     std::uint8_t& operator[](const int i) { return bgra[i]; }
-    TGAColor operator *(float f)  { return TGAColor(bgra[2] * f, bgra[1] * f, bgra[0] * f); }
-    TGAColor operator *(Vec3f f)  { return *this * (f[0] + f[1] + f[2]); }
-    TGAColor operator +(TGAColor c)  { return TGAColor(bgra[2] + c[2], bgra[1] + c[1], bgra[0] + c[0]); }
+
+    TGAColor operator *(float f)  {
+        float R = bgra[2] * f;
+        float G = bgra[1] * f;
+        float B = bgra[0] * f;
+
+        if (valid_range(R, G, B)) {
+            return TGAColor(R, G, B, bgra[3]);
+        } else {
+            std::cerr << "[Warn] color clamped to [0..255] in operator*()\n";
+            R = std::clamp<float>(R, 0, 255);
+            G = std::clamp<float>(G, 0, 255);
+            B = std::clamp<float>(B, 0, 255);
+            return TGAColor(R, G, B, bgra[3]);
+        }
+    }
+
+    TGAColor operator *(Vec3f f)  {
+        float R = bgra[2] * (f[0] + f[1] + f[2]);
+        float G = bgra[1] * (f[0] + f[1] + f[2]);
+        float B = bgra[0] * (f[0] + f[1] + f[2]);
+
+        if (valid_range(R, G, B)) {
+            return TGAColor(R, G, B, bgra[3]);
+        } else {
+            std::cerr << "[Warn] color clamped to [0..255] in operator*()\n";
+            R = std::clamp<float>(R, 0, 255);
+            G = std::clamp<float>(G, 0, 255);
+            B = std::clamp<float>(B, 0, 255);
+            return TGAColor(R, G, B, bgra[3]);
+        }
+    }
+
+    TGAColor operator +(TGAColor c)  {
+        int R = bgra[2] + c.bgra[2];
+        int G = bgra[1] + c.bgra[1];
+        int B = bgra[0] + c.bgra[0];
+
+        if (valid_range(R, G, B)) {
+            return TGAColor(R, G, B, bgra[3]);
+        } else {
+            std::cerr << "[Warn] color clamped to 255 in operator+()\n";
+            R = std::clamp(R, 0, 255);
+            G = std::clamp(G, 0, 255);
+            B = std::clamp(B, 0, 255);
+            return TGAColor(R, G, B, bgra[3]);
+        }
+    }
+
+    bool valid_range(int R, int G, int B) {
+        return (R >= 0 && R <= 255)
+               && (G >= 0 && G <= 255)
+               && (B >= 0 && B <= 255);
+    }
+
+    bool valid_range(float R, float G, float B) {
+        return (R >= 0.f && R <= 255.f)
+               && (G >= 0.f && G <= 255.f)
+               && (B >= 0.f && B <= 255.f);
+    }
+
+    friend std::ostream& operator<<(std::ostream& s, TGAColor &c) {
+        s << "R:" << int(c[2]) << ", " << "G:" << int(c[1]) << ", " << "B:" << int(c[2]) << ", " << "alpha:" << int(c[3]) << "\n";
+        return s;
+    }
 
     TGAColor() {}
     TGAColor(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 255, std::uint8_t bpp = 4)
